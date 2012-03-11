ee613b49ce57dc52ade95f4a6ab009857dd4916c
Andrew Szeto <andrew@jabagawee.com>
2012-03-11 14:11:44 -0700

    i forgot to add this?

--- 3.py
+++ 3.py
@@ -1,12 +1,4 @@
-from math import sqrt
-
-def is_prime(n):
-    if n % 2 == 0:
-        return False
-    for x in xrange(3, int(sqrt(n)) + 1, 2):
-        if n % x == 0:
-            return False
-    return True
+from util import is_prime
 
 num = 600851475143
 while not is_prime(num):
