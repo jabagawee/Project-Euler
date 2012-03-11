d8862dafb7df08854a486928f8c15bca34670518
Andrew Szeto <andrew@jabagawee.com>
2012-03-11 15:22:22 -0700

    needs moar memos

--- util.py
+++ util.py
@@ -25,3 +25,28 @@ def prime_factorize(n):
                 n /= x
                 break
     return factors
+
+import functools
+
+class memoize(object):
+   # stolen shamelessly from python.org
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      try:
+         return self.cache[args]
+      except KeyError:
+         value = self.func(*args)
+         self.cache[args] = value
+         return value
+      except TypeError:
+         # uncachable -- for instance, passing a list as an argument.
+         # Better to not cache than to blow up entirely.
+         return self.func(*args)
+   def __repr__(self):
+      """Return the function's docstring."""
+      return self.func.__doc__
+   def __get__(self, obj, objtype):
+      """Support instance methods."""
+      return functools.partial(self.__call__, obj)
