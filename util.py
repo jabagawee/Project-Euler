217577e77997c583cc2e7c38fde9ff073cd368ef
Andrew Szeto <andrew@jabagawee.com>
2012-03-11 14:26:28 -0700

    glaring error in my is_prime() code, amazed it didn't cause a problem before

--- util.py
+++ util.py
@@ -3,6 +3,8 @@ from math import sqrt
 def is_prime(n):
     if n < 2:
         return False
+    if n == 2:
+        return True
     if n % 2 == 0:
         return False
     for x in xrange(3, int(sqrt(n)) + 1, 2):
