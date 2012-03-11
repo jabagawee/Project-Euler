54d5dd0073654d929cb922d93854431e2f28b042
Andrew Szeto <andrew@jabagawee.com>
2012-03-11 14:47:11 -0700

    moved prime_factorize() to util.py

--- 5.py
+++ 5.py
@@ -1,16 +1,5 @@
 from collections import Counter
-
-def prime_factorize(n):
-    if n == 1:
-        return Counter({1: 1})
-    factors = Counter()
-    while n != 1:
-        for x in xrange(2, n+1):
-            if n % x == 0:
-                factors[x] += 1
-                n /= x
-                break
-    return factors
+from util import prime_factorize
 
 final_factors = Counter()
 for x in xrange(2, 20+1):
--- util.py
+++ util.py
@@ -11,3 +11,17 @@ def is_prime(n):
         if n % x == 0:
             return False
     return True
+
+from collections import Counter
+
+def prime_factorize(n):
+    if n == 1:
+        return Counter({1: 1})
+    factors = Counter()
+    while n != 1:
+        for x in xrange(2, n+1):
+            if n % x == 0:
+                factors[x] += 1
+                n /= x
+                break
+    return factors
