b6c712603a9e7ebcdd5b5c9ff69266ac5f11afb7
Andrew Szeto <andrew@jabagawee.com>
2012-03-13 16:35:22 -0700

    problem 206

---
+++ 206.py
@@ -0,0 +1,27 @@
+for x in xrange(101010103, 138902653+1, 10):
+    n = str(x*x)
+    if n[0] == '1' and \
+       n[2] == '2' and \
+       n[4] == '3' and \
+       n[6] == '4' and \
+       n[8] == '5' and \
+       n[10] == '6' and \
+       n[12] == '7' and \
+       n[14] == '8' and \
+       n[16] == '9':
+        print 10*x
+        break
+
+for x in xrange(101010107, 138902657+1, 10):
+    n = str(x*x)
+    if n[0] == '1' and \
+       n[2] == '2' and \
+       n[4] == '3' and \
+       n[6] == '4' and \
+       n[8] == '5' and \
+       n[10] == '6' and \
+       n[12] == '7' and \
+       n[14] == '8' and \
+       n[16] == '9':
+        print 10*x
+        break
