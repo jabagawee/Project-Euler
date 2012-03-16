a78b529311c01d35069af3ff4243ec0ac427f87a
Andrew Szeto <andrew@jabagawee.com>
2012-03-16 09:30:46 -0700

    problem 28

--- 28.py
+++ 28.py
@@ -1,1 +1,9 @@
-spiral = [[0 for _ in xrange(1001)] for _ in xrange(1001)]
+ans, curr = 1, 1
+SIZE = 1001
+
+for x in xrange(1, (SIZE - 1) / 2 + 1):
+    for y in xrange(4):
+        curr += 2*x
+        ans += curr
+
+print ans
