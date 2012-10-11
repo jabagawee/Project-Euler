6a1473ee14edc4c06c03783a283517c0243e56e4
Andrew Szeto <andrew@jabagawee.com>
2012-10-11 13:07:27 -0700

    a bit hackish to check for "the", but it works

--- 59.py
+++ 59.py
@@ -2,13 +2,15 @@ from itertools import izip
 
 f = map(int, open("cipher1.txt").read().split(','))
 
+
 def main():
     for a in xrange(ord('a'), ord('z') + 1):
         for b in xrange(ord('a'), ord('z') + 1):
             for c in xrange(ord('a'), ord('z') + 1):
-                key = [a, b, c] 
-                key *= len(f)/len(key) + 1
-                plaintext = ''.join(map(chr,[x ^ y for x, y in izip(f, key)]))
+                key = [a, b, c]
+                key *= len(f) / len(key) + 1
+                plaintext = ''.join(map(chr, [x ^ y for x, y in izip(f, key)]))
+                # check a very common pattern in english text
                 if ' the ' in plaintext:
                     print plaintext
                     print sum([ord(x) for x in plaintext])
