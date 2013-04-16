b559b67155733b968e1e8356e683ad10e184fe72
Andrew Szeto <andrew@jabagawee.com>
2013-04-16 10:53:23 -0700

    C is really fucking fast yay

---
+++ c/092.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_square_digits(int num) {
+    int ret = 0;
+    do {
+        int digit = num % 10;
+        ret += digit * digit;
+        num /= 10;
+    } while (num > 0);
+    return ret;
+}
+
+int main() {
+    int ans = 0;
+    int *cache;
+    cache = malloc (sizeof(int) * 10000003);
+    for (int i = 0; i < 10000003; i++) {
+        cache[i] = 0;
+    }
+    cache[1] = 1;
+    cache[89] = 89;
+
+    int temp;
+
+    for (int i = 2; i <= 10000000; i++) {
+        temp = i;
+        while (cache[temp] == 0) {
+            temp = sum_square_digits(temp);
+        }
+        cache[i] = cache[temp];
+        if (cache[i] == 89) {
+            ans++;
+        }
+    }
+
+    printf("%d\n", ans);
+    return 0;
+}
